3a9aa569850494fdef7b2010c10f8434c1d694e1
Debora Monego <debora.monego@gmail.com>
2024-10-16 14:17:53 +0200

    Updates

---
+++ precache-manifest.65e15cf65e88d9a706c321d49bc0dc5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ee024ed297f63241cb36bc09ca9e7f",
+    "url": "/collagen-infographic/index.html"
+  },
+  {
+    "revision": "37f7a9fa518871099165",
+    "url": "/collagen-infographic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a7ecd0eb948c0fed2f5d",
+    "url": "/collagen-infographic/static/js/2.f424be70.chunk.js"
+  },
+  {
+    "revision": "036a5bc5cca3900d5504",
+    "url": "/collagen-infographic/static/js/3.41c607a9.chunk.js"
+  },
+  {
+    "revision": "37f7a9fa518871099165",
+    "url": "/collagen-infographic/static/js/main.590f5fc8.chunk.js"
+  },
+  {
+    "revision": "fbf5fc1a8d05a5e33a95",
+    "url": "/collagen-infographic/static/js/runtime~main.14bfce0f.js"
+  }
+]);
